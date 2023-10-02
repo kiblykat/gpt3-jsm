2684c451c5cbdb3b8aa469803d11dfecfb6f068b
izzat <izzatfadzlon@gmail.com>
2023-10-02 15:00:14 +0800

    settled file and folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import reactDOM from "react-dom"
+import ReactDOM from "react-dom";
 
-import App from './App'
+import App from "./App";
+import './index.css'
 
-reactDOM.render(<App />,document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
+
