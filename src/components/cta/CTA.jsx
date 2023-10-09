407319905ca250712606b839d949d48301958a65
izzat <izzatfadzlon@gmail.com>
2023-10-10 00:33:21 +0800

    done:CTA no refer, todo: x-refer with vid answer

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,17 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='CTA-container'>
+      <div className='CTA-box'>
+        <div className='CTA-box__text'>
+          <p>Request Early Access to Get Started</p>
+          <h4>Register today & start exploring the endless possiblities.</h4>
+        </div>
+        <div className='CTA-box__button'>
+          <button>Get Started</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
