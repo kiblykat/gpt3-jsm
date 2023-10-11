fab8f0571d4d4b89d4792327d295d624bbc13ea7
izzat <izzatfadzlon@gmail.com>
2023-10-11 08:17:40 +0800

    in prog: Article component for Blog container

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './article.css'
+import { blog01,blog02, blog03, blog04, blog05} from './imports'
 
 const Article = () => {
   return (
-    <div>Article</div>
+    <div className='art-container'>
+      <img src={blog01} />
+      <p>Sep 26, 2021</p>
+      <h2>GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
+      <p>Read Full Article</p>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './blog.css'
+import Article from '../../components/article/Article'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3___blog section__padding'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          Article
+          Article
+          Article
+          Article
+        </div>
+      </div>
+    </div>
   )
 }
 
