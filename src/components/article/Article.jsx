d426af116e2d5e4184342827664cc3061c780dfe
muhammad.izzat.2.fadzlon@continental-corporation.com <muhammad.izzat.2.fadzlon@continental-corporation.com>
2023-10-12 09:45:56 +0800

    done: blog container, todo:footer

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './article.css'
-import { blog01,blog02, blog03, blog04, blog05} from './imports'
 
-const Article = () => {
+const Article = (props) => {
   return (
-    <div className='art-container'>
-      <img src={blog01} />
-      <p>Sep 26, 2021</p>
-      <h2>GPT-3 and Open  AI is the future. Let us exlore how it is?</h2>
-      <p>Read Full Article</p>
+    <div className='gpt__blog-container_article'>
+      <div className='gpt__blog-container_article-image'>
+        <img src={props.imgUrl} alt='blog' />
+      </div>
+      <div className='gpt__blog-container_article-content'>
+        <div>
+          <p>{props.date}</p>
+          <h3>{props.title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
   )
 }
